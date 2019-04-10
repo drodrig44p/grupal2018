40aaa6842820fcbec02d9629c34a1fead1406f0b
dawidh15 <dawidh15@gmail.com>
2019-04-10 10:06:31 -0600

    Archivos para informe 2019

--- Informe/vbgf.R
+++ Informe/vbgf.R
@@ -255,4 +255,73 @@ Viabilidadcambute <- transStochMat$new(
 )
 
 
+### MULTISEASON DATA EXAMPLE
 
+# 
+# n <- 50   # number of sites
+# T <- 4    # number of primary periods
+# J <- 3    # number of secondary periods
+# 
+# site <- 1:50
+# years <- data.frame(matrix(rep(2010:2013, each=n), n, T))
+# years <- data.frame(lapply(years, as.factor))
+# occasions <- data.frame(matrix(rep(1:(J*T), each=n), n, J*T))
+# 
+# y <- matrix(0:1, n, J*T)
+# 
+# umf <- unmarkedMultFrame(y=y,
+#                          siteCovs = data.frame(site=site),
+#                          obsCovs=list(occasion=occasions),
+#                          yearlySiteCovs=list(year=years),
+#                          numPrimary=T)
+# 
+# umfGMM1 <- unmarkedFrameGMM(y=y,
+#                             siteCovs = data.frame(site=site),
+#                             obsCovs=list(occasion=occasions),
+#                             yearlySiteCovs=data.frame(year=c(t(years))),
+#                             # or: yearlySiteCovs=list(year=years),
+#                             numPrimary=T, type="removal")
+# 
+# 
+# # A user-defined piFun calculating removal probs when time intervals differ.
+# instRemPiFun <- function(p) {
+#   M <- nrow(p)
+#   J <- ncol(p)
+#   pi <- matrix(NA, M, J)
+#   p[,1] <- pi[,1] <- 1 - (1 - p[,1])^2
+#   p[,2] <- 1 - (1 - p[,2])^3
+#   p[,3] <- 1 - (1 - p[,3])^5
+#   for(i in 2:J) {
+#     pi[,i] <- pi[, i - 1]/p[, i - 1] * (1 - p[, i - 1]) * p[, i]
+#   }
+#   return(pi)
+# }
+# 
+# # Associated obsToY matrix required by unmarkedFrameMPois
+# o2y <- diag(ncol(y))
+# o2y[upper.tri(o2y)] <- 1
+# o2y
+# 
+# 
+# umfGMM2 <- unmarkedFrameGMM(y=y,
+#                             siteCovs = data.frame(site=site),
+#                             obsCovs=list(occasion=occasions),
+#                             yearlySiteCovs=data.frame(year=years),
+#                             numPrimary=T, obsToY=o2y, piFun="instRemPiFun")
+# 
+# str
+# 
+# 
+# (m3 <- gmultmix(~1, ~1,~1, data=umfGMM1, K=30)) #no hay covariables 
+# backTransform(m2, type="lambda") # Individuals per plot
+# backTransform(m3, type="phi") # Probability of being avilable
+# (p <- backTransform(m3, type="det")) # Probability of detection
+# p <- coef(p)
+# # Multinomial cell probabilities under removal design
+# c(p, (1-p) * p, (1-p)^2 * p)
+# # Or more generally:
+# head(getP(m3))
+# # Empirical Bayes estimates of super-population size
+# re <- ranef(m3)
+# plot(re, layout=c(5,5), xlim=c(-1,20), subset=site%in%1:25)
+# 
